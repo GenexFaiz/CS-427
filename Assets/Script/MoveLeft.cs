d9c4a913ce940468211829d361723602084dbfd3
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2020-07-07 19:56:24 +0700

    Set up asset

---
+++ Assets/Script/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animation;
+    void Start()
+    {
+        animation.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
